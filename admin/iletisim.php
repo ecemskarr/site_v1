e5cc3093f19026bd894fe651dfdc9da734430a68
ecemskarr <ecemskarr@gmail.com>
2021-09-13 15:57:18 +0300

    iletişim sayfası güncelleme

--- admin/iletisim.php
+++ admin/iletisim.php
@@ -1,50 +1,47 @@
-
 <?php include 'include/header.php'; ?>
-    <section class="content-header">
-        <h1>
-            Dashboard
-            <small>Version 2.0</small>
-        </h1>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Dashboard</li>
-        </ol>
-    </section>
+<section class="content-header">
+    <h1>
+        Dashboard
+        <small>Version 2.0</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+    </ol>
+</section>
 <?php 
 $iletisim = DB::getRow("SELECT * FROM iletisim WHERE id=1");
-
-
 ?>
-    <!-- Main content -->
-    <section class="content">
-        <div class="col-md-12">
-            <div class="row">
-             <div class="box">
-<div class="box-header">İletişim Ayarları</div>
-<div class="box-body">
-<form action="" method="post">
-<div class="form-group">
-    <label > Telefon</label>
-    <textarea name="tel" class="form-control"><?=$iletisim->tel; ?></textarea>
-</div>
-<div class="form-group">
-    <label > E-mail</label>
-    <textarea name="email" class="form-control"><?=$iletisim->email; ?></textarea>
-</div>
-<div class="form-group">
-    <label > Adres </label>
-    <textarea name="adres" class="form-control"><?=$iletisim->adres; ?></textarea>
-</div>
-<div class="form-group">
-<button type="submit" class="btn btn-primary">Güncelle</button>
-</div>
-</form>
-</div>
-             </div>
+<!-- Main content -->
+<section class="content">
+    <div class="col-md-12">
+        <div class="row">
+            <div class="box">
+                <div class="box-header">İletişim Ayarları</div>
+                <div class="box-body">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label> Telefon</label>
+                            <textarea name="tel" class="form-control"><?=$iletisim->tel; ?></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label> E-mail</label>
+                            <textarea name="email" class="form-control"><?=$iletisim->email; ?></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label> Adres </label>
+                            <textarea name="adres" class="form-control"><?=$iletisim->adres; ?></textarea>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Güncelle</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
+    </div>
 
-    </section>
+</section>
 <?php 
 
 if($_POST){
@@ -70,5 +67,5 @@ if($_POST){
 
 
 
-    <!-- /.content -->
+<!-- /.content -->
 <?php include 'include/footer.php'; ?>
